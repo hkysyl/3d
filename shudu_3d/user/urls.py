6695000b435577234bdb8109e6adbaa523b3bf36
unknown <992978773@qq.com>
2020-06-16 16:09:40 +0800

    first commit

---
+++ shudu_3d/user/urls.py
@@ -0,0 +1,16 @@
+# from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+
+from user.views import login, register, revise, forget, index, upload,img
+
+urlpatterns = [
+    path('login/', login),
+    path('register/',register),
+    path('revise/',revise),
+    path('forget/',forget),
+    path('index/',index),
+    path('upload/',upload),
+    path('img/',img),
+
+]
